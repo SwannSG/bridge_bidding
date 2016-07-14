c13ab10673844ef9d8d80187fc2997f008d04296
SwannSG <swannsg@gmail.com>
2016-07-14 21:44:37 +0200

    modified:   client/main.js

--- client/main.js
+++ client/main.js
@@ -2,6 +2,7 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import './main.html';
 
+
 Meteor.startup(function() {
     console.log('startup');
     var adjustCardHeight = function() {
